03f4c8f05611abad11379febeb5a297544e10610
Gonza <gonzalososa342@gmail.com>
2024-06-09 23:26:25 -0300

    Basic layout with cdn styles

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+    />
+    <title>30 days to learn | Laravel</title>
+</head>
+<body>
+    <header>
+        @include('components.nav')
+    </header>
+    
+    <main>
+        {{ $slot }}
+    </main>
+
+    <style>
+        :root{
+            --padding-inline: 4rem;
+        }
+
+        header{
+            padding-inline: var(--padding-inline);
+        }
+        main{
+            padding-inline: var(--padding-inline);
+        }
+    </style>
+</body>
+</html>
