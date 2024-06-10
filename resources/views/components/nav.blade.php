7bf6c463b09f4b5852e71f6b2ce7b5f7c84fe897
Gonza <gonzalososa342@gmail.com>
2024-06-09 23:26:50 -0300

    Navigation component

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,19 @@
+<nav class="navigation">
+    <div>
+        <a href="/">Laravel</a>
+    </div>
+    <div>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact</a></li>
+        </ul>
+    </div>
+    <style>
+        nav{
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+    </style>
+</nav>
