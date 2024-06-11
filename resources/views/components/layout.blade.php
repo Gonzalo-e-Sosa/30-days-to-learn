6ddc673f3b0881f85f8b4dc246fff7196d14cdf8
Gonza <gonzalososa342@gmail.com>
2024-06-11 00:02:08 -0300

    Formatter changes

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,35 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
-    />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css" />
     <title>30 days to learn | Laravel</title>
 </head>
+
 <body>
+    @include('components.nav')
     <header>
-        @include('components.nav')
+        <div>
+            <h1>{{$heading}}</h1>
+        </div>
     </header>
-    
+
     <main>
-        {{ $slot }}
+        {{$slot}}
     </main>
 
     <style>
-        :root{
+        :root {
             --padding-inline: 4rem;
         }
 
-        header{
+        header {
             padding-inline: var(--padding-inline);
         }
-        main{
+
+        main {
             padding-inline: var(--padding-inline);
         }
     </style>
 </body>
+
 </html>
